f3bd777cd97b62d5ecb7e2d72edc7e543c5b01b3
amiparadis250 <pishimweaime7@gmail.com>
2024-11-25 07:12:34 +0200

    setted up seeders and models

--- Availsync/models.py
+++ Availsync/models.py
@@ -1,3 +1,51 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    profile_image = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'users'
+
+class Institution(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    totalstuffs = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    bio = models.CharField(max_length=200)
+    working_days = models.CharField(max_length=200)
+    working_hours = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'institutions'
+
+class Staff(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    profile_image = models.CharField(max_length=200)
+    role = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    workdescription = models.CharField(max_length=200)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'staffs'
